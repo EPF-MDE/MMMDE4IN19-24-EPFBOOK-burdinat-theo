9536ae5a5eaf91badb4a43dbe113a4ed8e3d0ff4
Theo Burdinat <theo.burdinat@epfedu.fr>
2024-03-08 17:50:54 +0100

    Automated reload functional

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! What a beautiful day today')
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
+  console.log(`This is so useful ! Waw`)
 })

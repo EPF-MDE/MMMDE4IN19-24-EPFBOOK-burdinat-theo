b4cf9e78a17ecc8edb6463516bc213e8f0242c13
Theo Burdinat <theo.burdinat@epfedu.fr>
2024-03-08 18:08:46 +0100

    First GET endpoint (/students)

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello World! What a beautiful day today')
 })
 
+app.get('/students', function (req, res) {
+  res.send([{ name: "Théo BURDINAT", school: "EPF"}, { name: "Manon VEDIE", school: "EPF"}, { name: "Juliette TORT", school: "EPF"}, { name: "Martin BOYTCHEV", school: "EPF"}, { name: "Thibaut MORIN", school: "EPF"}, { name: "Arthur BUONANNO", school: "EPF"}, { name: "Gaspard TIRFOIN", school: "EPF"}, { name: "Arnaud CARTON", school: "EPF"}, { name: "Abelle CHARLEMAGNE", school: "EPF"}, { name: "Anne-Zoé KASSIDONIS", school: "EPF"}])
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-  console.log(`This is so useful ! Waw`)
-})
+  })

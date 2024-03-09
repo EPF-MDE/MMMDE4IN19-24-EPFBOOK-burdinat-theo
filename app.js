d2515e148c1dcdd75a91afc742f9eea26fe4d3b4
Theo Burdinat <theo.burdinat@epfedu.fr>
2024-03-09 16:38:32 +0100

    The CSV is well returned as an array

--- app.js
+++ app.js
@@ -7,21 +7,27 @@ app.get('/', (req, res) => {
   res.send('Hello World! What a beautiful day today')
 })
 
-app.get('/students', function (req, res) {
-  res.send([{ name: "Théo BURDINAT", school: "EPF"}, { name: "Manon VEDIE", school: "EPF"}, { name: "Juliette TORT", school: "EPF"}, { name: "Martin BOYTCHEV", school: "EPF"}, { name: "Thibaut MORIN", school: "EPF"}, { name: "Arthur BUONANNO", school: "EPF"}, { name: "Gaspard TIRFOIN", school: "EPF"}, { name: "Arnaud CARTON", school: "EPF"}, { name: "Abelle CHARLEMAGNE", school: "EPF"}, { name: "Anne-Zoé KASSIDONIS", school: "EPF"}])
-})
 
-app.get('/studentsfromCSV', function (req, res) {
-  fs.readFile('students.csv', 'utf-8', (err, data) => {
-    if (err) throw err;
-    res.send(data);
+app.get("/students", (req, res) => {
+  const rowSeparator = "\n";
+  const cellSeparator = ","; 
+  fs.readFile("students.csv", "utf-8", (err, data) => {
+    const rows = data.split(rowSeparator);
+    const [headerRow, ...contentRows] = rows;
+    const header = headerRow.split(cellSeparator);
+    const students = contentRows.map((row) => {
+      const cells = row.split(cellSeparator);
+      const student = { 
+        [header[0]]: cells[0], 
+        [header[1]]: cells[1], 
+      };
+      return student; 
+    });
+    res.send(students); 
   });
-})
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-  fs.readFile('students.csv', 'utf-8', (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  });
   })

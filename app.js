0856ae87df796e759b1be31b5ab7fabed91c34cd
Theo Burdinat <theo.burdinat@epfedu.fr>
2024-03-14 19:36:57 +0100

    /api/* endpoint isolated with Express Router

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
+const apiRoute = require('./routes/api')
 const app = express()
 const port = 3000
 
@@ -10,33 +11,6 @@ app.use(express.static('public'));
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
-
-app.get("/api/students", (req, res) => {
-  const rowSeparator = "\r\n";
-  const cellSeparator = ","; 
-  fs.readFile("students.csv", "utf-8", (err, data) => {
-    const rows = data.split(rowSeparator);
-    const [headerRow, ...contentRows] = rows;
-    const header = headerRow.split(cellSeparator);
-    const students = contentRows.map((row) => {
-      const cells = row.split(cellSeparator);
-      const student = { 
-        [header[0]]: cells[0], 
-        [header[1]]: cells[1], 
-      };
-      return student; 
-    });
-    res.send(students); 
-  });
-});
-
-
-app.post("/api/students/create", (req, res) => {
-  console.log(req.body);
-  const csvLine = `\r\n${req.body.name},${req.body.school}`;
-  console.log(csvLine);
-  fs.writeFile("students.csv", csvLine, { flag: "a" }, (err) => { res.send("Student created");});
-})
 
 //TP2
 app.get("/", (req, res) => {
@@ -74,6 +48,8 @@ app.post("/students/create", (req, res) => {
   console.log(csvLine);
   fs.writeFile("students.csv", csvLine, { flag: "a" }, (err) => { res.send(res.redirect("/students/create?created=1"));});
 });
+
+app.use('/api', apiRoute);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ routes/api.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const fs = require('fs')
+const router = express.Router();
+const app = express()
+
+app.use(express.json())
+
+router.get("/students", (req, res) => {
+    const rowSeparator = "\r\n";
+    const cellSeparator = ","; 
+    fs.readFile("students.csv", "utf-8", (err, data) => {
+      const rows = data.split(rowSeparator);
+      const [headerRow, ...contentRows] = rows;
+      const header = headerRow.split(cellSeparator);
+      const students = contentRows.map((row) => {
+        const cells = row.split(cellSeparator);
+        const student = { 
+          [header[0]]: cells[0], 
+          [header[1]]: cells[1], 
+        };
+        return student; 
+      });
+      res.send(students); 
+    });
+});
+  
+  
+router.post("/students/create", (req, res) => {
+    console.log(req.body);
+    const csvLine = `\r\n${req.body.name},${req.body.school}`;
+    console.log(csvLine);
+    fs.writeFile("students.csv", csvLine, { flag: "a" }, (err) => { res.send("Student created");});
+});
+
+module.exports = router;

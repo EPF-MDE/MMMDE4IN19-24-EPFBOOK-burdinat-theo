18d3e52816aff93ebecced6e6a3f73f2bf89b5ec
Theo Burdinat <theo.burdinat@epfedu.fr>
2024-04-02 13:22:25 +0200

    Add the pie chart and the top 5 bar chart

--- public/students_data.js
+++ public/students_data.js
@@ -4,16 +4,24 @@ document.addEventListener("DOMContentLoaded", function(){
         console.log(data);
 
         let stressBustersCount = {};
+        let healthIssueCount = {};
         data.forEach(element => {
             const stressBusterName = element["Stress busters"];
+            const healthIssueBool = element["Health issue during lockdown"];
             if (typeof stressBustersCount[stressBusterName] === "undefined") {
                 stressBustersCount[stressBusterName] = 0;
             }
+            if (typeof healthIssueCount[healthIssueBool] === "undefined") {
+                healthIssueCount[healthIssueBool] = 0;
+            }
             stressBustersCount[stressBusterName] += 1;
+            healthIssueCount[healthIssueBool] += 1;
         });
+        stressBustersCount = Object.entries(stressBustersCount).sort((a,b) => b[1] - a[1]);
+        stressBustersCount = Object.fromEntries(stressBustersCount);
         console.log(stressBustersCount);
-        const counts = Object.values(stressBustersCount);
-        const categories = Object.keys(stressBustersCount);
+        const counts = Object.values(stressBustersCount).slice(0, 5);
+        const categories = Object.keys(stressBustersCount).slice(0, 5);
         const chart = c3.generate({
             bindto:"#chart",
             data: {
@@ -25,6 +33,14 @@ document.addEventListener("DOMContentLoaded", function(){
                     type: 'category',
                     categories: categories,
                 },
+            },
+        });
+        console.log(healthIssueCount);
+        const piechart = c3.generate({
+            bindto:"#pie",
+            data: {
+                columns: Object.entries(healthIssueCount),
+                type: 'pie',
             },
         });
     });

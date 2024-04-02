6021a683ed3831d3d38231d2523d4e65b562ece4
Theo Burdinat <theo.burdinat@epfedu.fr>
2024-04-02 12:48:11 +0200

    Our first bar chart

--- public/students_data.js
+++ public/students_data.js
@@ -2,14 +2,30 @@ document.addEventListener("DOMContentLoaded", function(){
     d3.select("body").style("background-color", "#e0f0f5");
     d3.csv("/covid-19-students-delhi-1.csv").then((data) => {
         console.log(data);
-    });
-    var chart = c3.generate({
-        bindto: '#chart',
-        data: {
-          columns: [
-            ['data1', 30, 200, 100, 400, 150, 250],
-            ['data2', 50, 20, 10, 40, 15, 25]
-          ]
-        }
+
+        let stressBustersCount = {};
+        data.forEach(element => {
+            const stressBusterName = element["Stress busters"];
+            if (typeof stressBustersCount[stressBusterName] === "undefined") {
+                stressBustersCount[stressBusterName] = 0;
+            }
+            stressBustersCount[stressBusterName] += 1;
+        });
+        console.log(stressBustersCount);
+        const counts = Object.values(stressBustersCount);
+        const categories = Object.keys(stressBustersCount);
+        const chart = c3.generate({
+            bindto:"#chart",
+            data: {
+                columns: [["Stress busters", ...counts]],
+                type: 'bar',
+            },
+            axis: {
+                x: {
+                    type: 'category',
+                    categories: categories,
+                },
+            },
+        });
     });
 });

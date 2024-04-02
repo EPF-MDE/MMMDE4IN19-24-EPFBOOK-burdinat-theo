1f7960586ec1457b8ee415c58420666bf72a29a7
Theo Burdinat <theo.burdinat@epfedu.fr>
2024-04-03 01:35:37 +0200

    Creation of a stacked bar plot to link health issues and Stress Buster

--- public/students_data.js
+++ public/students_data.js
@@ -2,9 +2,9 @@ document.addEventListener("DOMContentLoaded", function(){
     d3.select("body").style("background-color", "#e0f0f5");
     d3.csv("/covid-19-students-delhi-1.csv").then((data) => {
         console.log(data);
-
         let stressBustersCount = {};
         let healthIssueCount = {};
+        let healthIssueByStressBuster = {};
         data.forEach(element => {
             const stressBusterName = element["Stress busters"];
             const healthIssueBool = element["Health issue during lockdown"];
@@ -16,6 +16,13 @@ document.addEventListener("DOMContentLoaded", function(){
             }
             stressBustersCount[stressBusterName] += 1;
             healthIssueCount[healthIssueBool] += 1;
+            if (typeof healthIssueByStressBuster[stressBusterName] === "undefined") {
+                healthIssueByStressBuster[stressBusterName] = {};
+            }
+            if (typeof healthIssueByStressBuster[stressBusterName][healthIssueBool] === "undefined") {
+                healthIssueByStressBuster[stressBusterName][healthIssueBool] = 0;
+            }
+            healthIssueByStressBuster[stressBusterName][healthIssueBool] += 1;
         });
         stressBustersCount = Object.entries(stressBustersCount).sort((a,b) => b[1] - a[1]);
         stressBustersCount = Object.fromEntries(stressBustersCount);
@@ -41,6 +48,31 @@ document.addEventListener("DOMContentLoaded", function(){
             data: {
                 columns: Object.entries(healthIssueCount),
                 type: 'pie',
+            },
+        });
+        console.log(healthIssueByStressBuster);
+        healthIssueByStressBuster = Object.entries(healthIssueByStressBuster).sort((a, b) => {
+            const sumCounts = obj => Object.values(obj).reduce((HealthIssue1, HealthIssue2) => HealthIssue1 + HealthIssue2, 0);
+            const sumA = sumCounts(a[1]);
+            const sumB = sumCounts(b[1]);
+            return sumB - sumA;
+        });
+        healthIssueByStressBuster = Object.fromEntries(healthIssueByStressBuster);
+        const healthCounts = Object.values(healthIssueByStressBuster).slice(0, 5);
+        const healthCategories = Object.keys(healthIssueByStressBuster).slice(0, 5);
+        console.log(healthCounts);
+        const stackedChart = c3.generate({
+            bindto:"#stackedChart",
+            data: {
+                columns: [['No had health issue during lockdown', ...healthCounts.map(healthCount => healthCount["NO"])], ['Had health issue during lockdown', ...healthCounts.map(healthCount => healthCount["YES"])],],
+                type: 'bar',
+                groups: [['No had health issue during lockdown', 'Had health issue during lockdown']],
+            },
+            axis: {
+                x: {
+                    type: 'category',
+                    categories: healthCategories,
+                },
             },
         });
     });

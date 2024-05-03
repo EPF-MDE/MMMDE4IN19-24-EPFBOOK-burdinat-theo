031db748d9a96a331584462bbe311a0dbd086203
Theo Burdinat <theo.burdinat@epfedu.fr>
2024-05-03 22:49:53 +0200

    Made all API routes

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const { getFromCsvfile, storeStudentInCsvFile } = require('../csvfile_manipulation');
+const { getFromCsvfile, storeStudentInCsvFile, getUserFromId, updateUser } = require('../csvfile_manipulation');
 const router = express.Router();
+const StudentModel = require('../models/Student');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/epfbook');
 
 router.get("/students", (req, res) => {
   getFromCsvfile("students", (err, students) => {
@@ -36,5 +39,56 @@ router.post("/login", (req, res) => {
   console.log(req.cookies);
 });
 
+router.post("/students/create-in-db", (req, res) => {
+  const student = new StudentModel(req.body);
+  student.save().then((result) => {
+    res.send("Student created successfully");
+  }).catch((err) => {
+    res.send("Error creating student");
+  });
+});
+
+router.get("/students/from-db", async (req, res) => {
+  const students = await StudentModel.find({}).exec();
+  res.send(students);
+});
+
+router.get('/students/:id', (req, res) => {
+  const id = parseInt(req.params.id)+1;
+  const id_link = id-1;
+  student = getUserFromId(id, (err, student) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(student);
+    }
+  });
+});
+
+router.post('/students/:id', (req, res) => {
+  const id = parseInt(req.params.id)+1;
+  const {name, school} = req.body;
+  updateUser(id, name, school, (err) => {
+    if (err) {
+      console.log(err);
+      res.send("Failed to update student.");
+    } else {
+      res.send("Student updated successfully.");
+    }
+  });
+});
+
+router.put('/students/:id/update', (req, res) => {
+  const id = parseInt(req.params.id)+1;
+  const {name, school} = req.body;
+  updateUser(id, name, school, (err) => {
+    if (err) {
+      console.log(err);
+      res.send("Failed to update student.");
+    } else {
+      res.send("Student updated successfully.");
+    }
+  });
+});
 
 module.exports = router;
